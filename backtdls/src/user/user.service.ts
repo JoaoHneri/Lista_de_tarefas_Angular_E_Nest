da6907b5cb53ced22d646a9d1e8687e867ada4cc
João Henrique <joaohnsousa@gmail.com>
2023-11-30 23:22:08 -0300

    Introduzindo o bcrypt e encriptografando a senha

--- backtdls/src/user/user.service.ts
+++ backtdls/src/user/user.service.ts
@@ -2,13 +2,17 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
-
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class UserService {
-  constructor(private readonly prisma: PrismaService){}
-  
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const user = {
+      ...createUserDto,
+      password: await bcrypt.hash(createUserDto.password, 10),
+    };
+    return user;
   }
 
   findAll() {
