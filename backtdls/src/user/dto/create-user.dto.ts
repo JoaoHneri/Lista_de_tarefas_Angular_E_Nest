0e5ec915c5bbf7805b224d673f5e2dfeceed3a1b
João Henrique <joaohnsousa@gmail.com>
2023-11-30 23:00:15 -0300

    Definindo User Entity e Dto, criando serviços de criação de usuário

--- backtdls/src/user/dto/create-user.dto.ts
+++ backtdls/src/user/dto/create-user.dto.ts
@@ -1,1 +1,24 @@
-export class CreateUserDto {}
+import { User } from '../entities/user.entity';
+import {
+  IsEmail,
+  Matches,
+  MaxLength,
+  MinLength,
+  IsString,
+} from 'class-validator';
+export class CreateUserDto extends User {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'Senha muito fraca, sua senha deve conter pelo menos um dígito, ou conter pelo menos um caractere não alfanumérico, Não conter pontos (.) ou quebras de linha, deve conter pelo menos uma letra maiúscula e uma letra minúscula',
+  })
+  password: string;
+
+  @IsString()
+  name: string;
+}
