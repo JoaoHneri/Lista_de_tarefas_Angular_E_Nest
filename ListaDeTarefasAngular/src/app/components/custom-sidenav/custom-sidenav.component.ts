70d116883d67cf1404d02c7fbaa21833d680c018
João Henrique <joaohnsousa@gmail.com>
2023-12-05 19:30:09 -0300

    Função de Sair criada

--- ListaDeTarefasAngular/src/app/components/custom-sidenav/custom-sidenav.component.ts
+++ ListaDeTarefasAngular/src/app/components/custom-sidenav/custom-sidenav.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-custom-sidenav',
@@ -7,10 +8,16 @@ import { Component, Input } from '@angular/core';
 })
 
 export class CustomSidenavComponent {
+  constructor(private router: Router){}
   @Input()
   collapsed: boolean | undefined
   profilePicSize = (()=> this.collapsed ? '32' : '100')
 
+  logOut():void{
+    localStorage.clear();
+    this.router.navigate(['/login']);
+
+  }
   menuItems = [
     { icon: 'dashboard', label: 'Dashboard', route: 'dashboard' },
     { icon: 'calendar_month', label: 'Calendário', route: 'Calendar' },
